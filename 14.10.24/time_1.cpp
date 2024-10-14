deb4d73ccf0d5de3422eb050a96c2318b5b41ac4
Svetlana Khokholeva <57761906+Khokholeva@users.noreply.github.com>
2024-10-14 10:55:25 +0300

    Update and rename time_1_beta.cpp to time_1.cpp

--- 14.10.24/time_1.cpp
+++ 14.10.24/time_1.cpp
@@ -8,20 +8,19 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    using clock = std::chrono::system_clock;
-    time_t start = clock::to_time_t(clock::now());
-    start += 3600 * 3;
+    time_t t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    t += 3600 * 3;
     int h, m, s;
     while (true) {
-        s = start % 60;
-        m = (start / 60) % 60;
-        h = (start / 3600) % 24;
+        cout << '\r';
+        s = t % 60;
+        m = (t / 60) % 60;
+        h = (t / 3600) % 24;
         (h < 10 ? cout << '0' << h << ':' : cout << h << ':');
         (m < 10 ? cout << '0' << m << ':' : cout << m << ':');
         (s < 10 ? cout << '0' << s : cout << s);
-        cout << endl;
-        start++;
-        std::this_thread::sleep_for(990ms);
+        t++;
+        std::this_thread::sleep_for(995ms);
     }
     return EXIT_SUCCESS;
 }
