934f804ec4274669a34f7fed8f40d094e2764ee9
Svetlana Khokholeva <57761906+Khokholeva@users.noreply.github.com>
2024-11-18 14:07:18 +0300

    Update async_2.cpp

--- 18.11.24/async_2.cpp
+++ 18.11.24/async_2.cpp
@@ -3,8 +3,9 @@
 #include <chrono>
 #include <vector>
 #include <cmath>
-#include <fstream>
-#include <sstream>
+// для ввода текста в строку для поиска
+#include<fstream>
+#include <sstream> 
 using namespace std;
 using namespace std::chrono;
 
@@ -13,7 +14,7 @@ int block_size(int threads, int n)
 	return n / threads + (n % threads ? 1 : 0);
 }
 
-int Find(string s, string what, int a, int b) {
+int Find(string what, string s, int a, int b) {
 	int l = what.size();
 	string test;
 	int res = 0;
@@ -24,7 +25,15 @@ int Find(string s, string what, int a, int b) {
 	return res;
 }
 
-void MultiThreadFind(int threads, string s, string what)
+int Find_thread(string what, string s, int threads, int first_index){
+	int s_size = s.size();
+	int bl_size = block_size(threads, s_size);
+	int a = first_index;
+	int b = min(a + bl_size, s_size);
+	return Find(what, s, a, b);
+}
+
+void MultiThreadFind(string what, string s, int threads)
 {
 	vector<future <int>> fut(threads);
 	int s_size = s.size();
@@ -34,7 +43,7 @@ void MultiThreadFind(int threads, string s, string what)
 	int ans = 0;
 	while (first_index < s_size)
 	{
-		fut[i] = async(Find, s, what, first_index, min(first_index + bl_size, s_size));
+		fut[i] = async(Find_thread, what, s, threads, first_index);
 		first_index += bl_size;
 		i++;
 	}
@@ -42,10 +51,14 @@ void MultiThreadFind(int threads, string s, string what)
 		ans += fut[j].get();
 	cout << ans << endl;
 }
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+
 	string s = "", what = "the";
+
+	//ввод s
 	ifstream file("bible.txt");
 	string line;
 	while (std::getline(file, line))
@@ -53,10 +66,12 @@ int main()
 		s += line + " ";
 	}
 	cout << "Длина строки: " << s.size() << endl << endl;
+
+
 	cout << "1 thread" << endl;
 	int threads = 1;
 	auto start = steady_clock::now();
-	MultiThreadFind(threads, s, what);
+	MultiThreadFind( what, s, threads);
 	auto finish = steady_clock::now();
 
 	cout << duration_cast <milliseconds> (finish - start).count() << "ms " << endl << endl;
@@ -64,7 +79,7 @@ int main()
 	cout << "8 threads" << endl;
 	threads = 8;
 	start = steady_clock::now();
-	MultiThreadFind(threads, s, what);
+	MultiThreadFind(what, s, threads);
 	finish = steady_clock::now();
 
 	cout << duration_cast <milliseconds> (finish - start).count() << "ms " << endl;
